f4ce5489897010b1bd0ffa2dcbcc9dcce0ec695e
Yunji Jong <yunji.jong@yale.edu>
2019-05-24 10:57:48 -0400

    change

--- PyrPopfunc.py
+++ PyrPopfunc.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def PlotPyr(data_list, xlim, ylim, title):
+def PlotPyr(data_list, numSim, xlim, ylim, title):
     men_count = 0
     women_count = 0
     womens_list = []
@@ -12,39 +12,65 @@ def PlotPyr(data_list, xlim, ylim, title):
 
     length = len(data_list)
     i = 0
+    women_count = 0
+    men_count = 0
+    m_num_age = []
+    w_num_age = []
     while i < length:
         if data_list[i][1] == 1:
-            womens_list.insert(women_count, data_list[i][2])
+            womens_list.insert(women_count, data_list[i][2] * 100)
             w_age.insert(women_count, data_list[i][0])
             women_count = women_count + 1
             i = i + 1
         else:
-            mens_list.insert(men_count, data_list[i][2])
+            mens_list.insert(men_count, data_list[i][2] * 100)
             m_age.insert(men_count, data_list[i][0])
             men_count = men_count + 1
             i = i + 1
 
-
     fig, axis= plt.subplots(ncols = 2, sharey = True, tight_layout=True)
-    fig.suptitle(title)
+    st = fig.suptitle(title)
     plt.setp(axis, yticks=[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100],
     yticklabels=['0-4', '5-9', '10-14', '15-19', '20-24', '25-29', '30-34', '35-39', '40-44', '45-49', '50-54', '55-59', '60-64', '65-69', '70-74',
     '75-79', '80-89', '90-94', '95-100', '100+'])
-    axis[0].xmin, axis[0].xmax, axis[0].ymin, axis[0].ymax = axis[0].axis([0, xlim, 0, ylim])
-    axis[1].xmin, axis[1].xmax, axis[1].ymin, axis[1].ymax = axis[1].axis([0, xlim, 0, ylim])
-    axis[0].barh(m_age, mens_list, width, align = 'edge', color = 'brown', edgecolor = 'black')
+    axis[0].xmin, axis[0].xmax, axis[0].ymin, axis[0].ymax = axis[0].axis([0, xlim, -0.5, ylim])
+    axis[1].xmin, axis[1].xmax, axis[1].ymin, axis[1].ymax = axis[1].axis([0, xlim, -2.5, ylim])
+    axis[0].barh(m_age, mens_list, width, align = 'center', color = 'brown', edgecolor = 'black')
     axis[0].invert_xaxis()
     axis[0].set(title = 'Men')
-    axis[1].barh(w_age, womens_list, width, align = 'edge', color = 'gray', edgecolor = 'black')
+    axis[1].barh(w_age, womens_list, width, align = 'center', color = 'gray', edgecolor = 'black')
     axis[1].set(title = 'Women')
     axis[0].set_ylabel('Age')
     axis[0].set_xlabel('Percent of Population')
     axis[1].set_xlabel('Percent of Population')
 
+    #accounting for numSim
+    men_count = 0
+    women_count = 0
+    if len(numSim) > 1:
+        num_length = len(numSim)
+        j = 0
+        k = 0
+        while j < num_length:
+            while k < len(numSim[j]):
+                if numSim[j][k][1] == 1:
+                    axis[0].plot(numSim[j][k][2] * 100, numSim[j][k][0], marker = 'o', markersize = 3, color = "blue")
+                    #women_count = women_count + 1
+                    k = k + 1
+                else:
+                    axis[1].plot(numSim[j][k][2] * 100, numSim[j][k][0], marker = 'o', markersize = 3, color = "blue")
+                    #men_count = women_count + 1
+                    k = k + 1
+            j = j + 1
+
+
+    fig.tight_layout()
+    st.set_y(1.0)
+    fig.subplots_adjust(top=0.6)
     return fig
 
-
-
 data_table = [[0, 0, 0.1], [0, 1, 0.2], [5, 0, 0.3], [5, 1, 0.4], [10, 0, 0.6], [10, 1, 0.4]]
-pyrfig = PlotPyr(data_table, 0.7, 20, 'Pop')
+num_table = [[[0, 0, 0.5], [0, 1, 0.3], [5, 0, 0.5], [5, 1, 0.6]],
+[[0, 0, 0.2], [0, 1, 0.4], [5, 0, 0.9], [5, 1, 0.4]]]
+pyrfig = PlotPyr(data_table, num_table, 70, 20, 'Population Pyramid')
 plt.show(pyrfig)
